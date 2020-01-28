2e7c8092a29f7918899a9747811bcb3e03580e30
fausto <fausto.alemao@gmail.com>
2020-01-28 10:37:06 -0300

    Home view implemented

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -3,18 +3,31 @@ import {Text, View, TouchableOpacity} from 'react-native';
 import styles from "./styles";
 
 class Home extends Component {
-  render = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to the Trivia Challenge!</Text>
-        <Text>You will be presented with 10 True or False questions.</Text>
-        <Text>Can you score 100%?</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text>BEGIN</Text>
-        </TouchableOpacity>
-      </View>
-      );
-  }
+  render = () => (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the{'\n'}Trivia Challenge!</Text>
+      <Text style={styles.explanation}>You will be presented with 10 True or False questions.</Text>
+      <Text style={styles.explanation}>Can you score 100%?</Text>
+      <TouchableOpacity style={styles.button} onPress={()=>this.props.navigation.navigate('Quiz')}>
+        <Text style={styles.buttontitle}>BEGIN</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 export default Home
+
+/*
+const home = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Welcome to the{'\n'}Trivia Challenge!</Text>
+    <Text style={styles.explanation}>You will be presented with 10 True or False questions.</Text>
+    <Text style={styles.explanation}>Can you score 100%?</Text>
+    <TouchableOpacity style={styles.button} onPress={()=>this.props.navigation.navigate('Quiz')}>
+      <Text style={styles.buttontitle}>BEGIN</Text>
+    </TouchableOpacity>
+  </View>
+)
+
+export default home
+*/
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -3,12 +3,37 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   button: {
     fontSize: 20,
-    padding: 5
+    padding: 5,
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    width: 250, 
+    borderColor: '#3897f0', 
+    borderWidth: 1, 
+    backgroundColor: '#3897f0', 
+    borderRadius: 5,
+    color: '#fff',
+    height:40
+  },
+  buttontitle:{
+    fontWeight: 'bold', 
+    color: '#fff', 
+    fontSize: 18
   },
   container: {
     flex:1,
+    margin:10,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-between",
+    flexDirection:'column',
+  },
+  explanation:{
+    textAlign:'center',
+    fontSize:24
+  },
+  title: {
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize:32
   },
   box: {
     justifyContent: "center",
